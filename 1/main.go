c797a2496706bdcd4c2c689e50f851a4b7d92411
Marta Paciorkowska <marta.paciorkowska@finn.no>
2020-12-01 13:13:26 +0100

    Remove one loop, optimize!

--- 1/main.go
+++ 1/main.go
@@ -13,18 +13,13 @@ func main() {
 		log.Fatalf("could not open file: %s", err)
 	}
 	defer file.Close()
-	var inputNumbers []int64
+	numberPairs := make(map[int64]int64)
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		number, err := strconv.ParseInt(scanner.Text(), 0, 64)
 		if err != nil {
 			log.Fatalf("could not parse number: %s", err)
 		}
-		inputNumbers = append(inputNumbers, number)
-	}
-
-	numberPairs := make(map[int64]int64)
-	for _, number := range inputNumbers {
 		numberPairs[number] = (2020 - number)
 	}
 
