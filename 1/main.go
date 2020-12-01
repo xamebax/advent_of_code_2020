197d554b667ebda7958d742705036d50ca62a91c
Marta Paciorkowska <marta.paciorkowska@finn.no>
2020-12-01 13:58:44 +0100

    Second task of Dec 1

--- 1/main.go
+++ 1/main.go
@@ -8,12 +8,53 @@ import (
 )
 
 func main() {
+	findTwoEntriesThatSumUpTo2020()
+	findThreeEntriesThatSumUpTo2020()
+}
+
+func openFile(filePath string) *os.File {
 	file, err := os.Open("input")
 	if err != nil {
 		log.Fatalf("could not open file: %s", err)
+		return nil
 	}
+	return file
+}
+
+func findThreeEntriesThatSumUpTo2020() {
+	file := openFile("input")
 	defer file.Close()
+
+	var inputNumbers []int64
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		number, err := strconv.ParseInt(scanner.Text(), 0, 64)
+		if err != nil {
+			log.Fatalf("could not parse number: %s", err)
+		}
+		inputNumbers = append(inputNumbers, number)
+	}
+
+MegaLoop:
+	for _, firstNumber := range inputNumbers {
+		for _, secondNumber := range inputNumbers {
+			for _, thirdNumber := range inputNumbers {
+				if firstNumber+secondNumber+thirdNumber == 2020 {
+					log.Printf("The product of three entries that sum up to 2020 is: %d", firstNumber*secondNumber*thirdNumber)
+					break MegaLoop
+				}
+			}
+		}
+	}
+}
+
+func findTwoEntriesThatSumUpTo2020() {
+	file := openFile("input")
+	defer file.Close()
+
 	numberPairs := make(map[int64]int64)
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		number, err := strconv.ParseInt(scanner.Text(), 0, 64)
@@ -25,7 +66,7 @@ func main() {
 
 	for _, number := range numberPairs {
 		if numberPairs[number]+number == 2020 {
-			log.Printf("The winning number is: %d", numberPairs[number]*number)
+			log.Printf("The product of two entries that sum up to 2020 is: %d", numberPairs[number]*number)
 			break
 		}
 	}
